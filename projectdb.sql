d287ebb67668f93259bfcbe4b1579cfd0db00729
HomelessNoob <michele.zhu@mail.polimi.it>
2021-02-20 17:19:48 +0100

    upup

--- projectdb.sql
+++ projectdb.sql
@@ -32,8 +32,8 @@ CREATE TABLE `review` (
 PRIMARY KEY (`id`),
 KEY `user_idR` (`user`),
 KEY `questionnaire_idR` (`questionnaire`),
-CONSTRAINT `questionnaire_idR` FOREIGN KEY (`questionnaire`) REFERENCES `questionnaire` (`id`),
-CONSTRAINT `user_idR` FOREIGN KEY (`user`) REFERENCES `user` (`id`) 
+CONSTRAINT `questionnaire_idR` FOREIGN KEY (`questionnaire`) REFERENCES `questionnaire` (`id`) ON DELETE CASCADE,
+CONSTRAINT `user_idR` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=272 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
@@ -43,7 +43,7 @@ CREATE TABLE `question` (
 `questionnaire` int NOT NULL,
 PRIMARY KEY (`id`),
 KEY `questionnaire_idQ` (`questionnaire`),
-CONSTRAINT `questionnaire_idQ` FOREIGN KEY (`questionnaire`) REFERENCES `questionnaire` (`id`)
+CONSTRAINT `questionnaire_idQ` FOREIGN KEY (`questionnaire`) REFERENCES `questionnaire` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=272 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
@@ -54,8 +54,8 @@ CREATE TABLE `answer` (
 KEY `question_idA` (`question`),
 KEY `review_idA` (`review`),
 PRIMARY KEY (`review`,`question`),
-CONSTRAINT `question_idA` FOREIGN KEY (`question`) REFERENCES `question` (`id`) ,
-CONSTRAINT `review_idA` FOREIGN KEY (`review`) REFERENCES `review` (`id`) 
+CONSTRAINT `question_idA` FOREIGN KEY (`question`) REFERENCES `question` (`id`) ON DELETE CASCADE,
+CONSTRAINT `review_idA` FOREIGN KEY (`review`) REFERENCES `review` (`id`) ON DELETE CASCADE
 )ENGINE=InnoDB AUTO_INCREMENT=272 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
--- projectdbV1.sql
+++ projectdbV1.sql
@@ -44,8 +44,8 @@ CREATE TABLE `review` (
 PRIMARY KEY (`id`),
 KEY `user_idR` (`user`),
 KEY `questionnaire_idR` (`questionnaire`),
-CONSTRAINT `questionnaire_idR` FOREIGN KEY (`questionnaire`) REFERENCES `questionnaire` (`id`),
-CONSTRAINT `user_idR` FOREIGN KEY (`user`) REFERENCES `user` (`id`) 
+CONSTRAINT `questionnaire_idR` FOREIGN KEY (`questionnaire`) REFERENCES `questionnaire` (`id`) ON DELETE CASCADE,
+CONSTRAINT `user_idR` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
@@ -55,7 +55,7 @@ CREATE TABLE `question` (
 `questionnaire` int NOT NULL,
 PRIMARY KEY (`id`),
 KEY `questionnaire_idQ` (`questionnaire`),
-CONSTRAINT `questionnaire_idQ` FOREIGN KEY (`questionnaire`) REFERENCES `questionnaire` (`id`)
+CONSTRAINT `questionnaire_idQ` FOREIGN KEY (`questionnaire`) REFERENCES `questionnaire` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
@@ -66,8 +66,8 @@ CREATE TABLE `answer` (
 KEY `question_idA` (`question`),
 KEY `review_idA` (`review`),
 PRIMARY KEY (`review`,`question`),
-CONSTRAINT `question_idA` FOREIGN KEY (`question`) REFERENCES `question` (`id`) ,
-CONSTRAINT `review_idA` FOREIGN KEY (`review`) REFERENCES `review` (`id`) 
+CONSTRAINT `question_idA` FOREIGN KEY (`question`) REFERENCES `question` (`id`) ON DELETE CASCADE,
+CONSTRAINT `review_idA` FOREIGN KEY (`review`) REFERENCES `review` (`id`) ON DELETE CASCADE
 )ENGINE=InnoDB AUTO_INCREMENT=272 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
